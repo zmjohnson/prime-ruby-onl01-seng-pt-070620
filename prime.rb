b3ad098ca657fb371559138592942f33f75b40a3
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-11 18:21:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,9 @@ def prime?(number)
   if number > 1
     range = (start..number-1).to_a
     range.none? do |num_to_test|
-      
+  number % num_to_test == 0
+    end
+  else
+    false    
     end
   end
